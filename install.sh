565dd37bd97dc2ace2a37dfdc4e54671e8342b92
vindard <17693119+vindard@users.noreply.github.com>
2019-12-16 09:59:36 -0400

    consilidate `cleanup.sh`; remove problematic exec command
    The command "$ exec "/usr/bin/zsh" was starting new shells
    and not having the desired effect. It is probably better
    to run this command separately outside the script.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-
-# Install setting files from repo
+###########
+# INSTALL #
+###########
 
 SETUP_FILES=(.commonrc .bash_aliases)
 
@@ -33,5 +34,50 @@ for CONFIG_FILE in "${CONFIG_FILES[@]}"; do
 	fi
 done
 
-exec "$SHELL"
+
+###########
+# CLEANUP #
+###########
+
+CWD=$(pwd)
+TEMPFILE=temp.txt
+
+SETUP_FILES=(.commonrc .bash_aliases)
+
+function produce_diff {
+        git diff --color ~/$SETUP_FILE.bak ~/$SETUP_FILE > $TEMPFILE
+        echo
+        echo
+        echo "<-- START DIFF -->"
+        echo
+        cat $TEMPFILE
+        echo
+        echo "<-- END DIFF -->"
+        echo
+        echo
+        rm $TEMPFILE
+}
+
+function cleanup_file {
+        produce_diff
+        read -p "Would you like to cleanup $SETUP_FILE.bak file? (y/n): " RESPONSE
+        if [ "$RESPONSE" == "y" ]; then
+                echo "Removing $SETUP_FILE.bak..."
+                rm ~/$SETUP_FILE.bak
+        fi
+}
+
+function run {
+        echo "Starting cleanup..."
+        echo
+        for SETUP_FILE in "${SETUP_FILES[@]}"; do
+                if [ -f "$SETUP_FILE.bak" ]; then
+                        cleanup_file
+                fi
+        done
+        echo "Cleanup finished!"
+}
+
+cd && run
+cd $CWD
 
